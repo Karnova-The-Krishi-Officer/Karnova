33e0561c6967001c92a84997ee97792c9a329cd9
Pravith P Kotian <official.pravith@gmail.com>
2026-03-29 10:51:14 +0530

    Revert "Karnova frontend: modular app shell, offline-first sync, role panels, theming, and lazy loading"

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {
-      // registration failure is non-blocking for app usage
-    });
-  });
-}
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
